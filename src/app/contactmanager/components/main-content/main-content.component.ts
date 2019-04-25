bfd7c6bc73df2347091d714026b28ac92f13df18
Ktojan <karpovich.andrey@gmail.com>
2019-04-25 12:38:11 +0200

    inserted mechanism show-hide markers

--- src/app/contactmanager/components/main-content/main-content.component.ts
+++ src/app/contactmanager/components/main-content/main-content.component.ts
@@ -11,7 +11,6 @@ import { UserService } from "../../services/user.service";
 export class MainContentComponent implements OnInit {
 
     user: User;
-    map: any;
 
     constructor(private route: ActivatedRoute,
         private service: UserService) {
@@ -19,6 +18,12 @@ export class MainContentComponent implements OnInit {
   }
 
     ngOnInit() {
+
+        const DG = window.DG;
+        const map = DG.map('map', {
+            'center': [46.473025, 30.74104], // Географическая точка с определенной широтой и долготой.
+            'zoom': 13
+        });
 
       this.route.params.subscribe(params => {
           let id = params['id'];
@@ -29,8 +34,8 @@ export class MainContentComponent implements OnInit {
               this.user = this.service.userById(id);
           })
       })
-      var myIcon = window.DG.icon({
-          iconUrl: '../../assets/golf_marker.png',
+      var myIcon = DG.icon({
+          iconUrl: '../../assets/marker_image.png',
           //iconRetinaUrl: 'my-icon@2x.png',
           iconSize: [28, 35],
           //iconAnchor: [22, 94],
@@ -40,26 +45,45 @@ export class MainContentComponent implements OnInit {
           //shadowSize: [68, 95],
           //shadowAnchor: [22, 94]
       });
+     
 
-      this.map = window.DG.map('map', {
-          'center': [46.473025, 30.74104], // Географическая точка с определенной широтой и долготой.
-          'zoom': 13
-      });
-
-      this.map.on('click', function (e) {
-          console.dir(e.latlng.lat + ', long: ' + e.latlng.lng);
-      });
-
+      
         //Adding markers with rigth-mouse click
-      this.map.on('contextmenu', function(e) {
-          console.log(this);
+      map.on('contextmenu', function(e) {
           console.log(e);
-          window.DG.marker([e.latlng.lat, e.latlng.lng], { icon: myIcon }).addTo(this).bindPopup('contextmenu');
+          DG.marker([e.latlng.lat, e.latlng.lng]).addTo(this).bindPopup('contextmenu');
       });
 
 
-      //let m1 = window.DG.marker([46.473027, 30.74107], { icon: myIcon }).addTo(this.map);
-     // let m2 = window.DG.marker([46.473038, 30.74159], { icon: myIcon }).addTo(this.map);
+      //let group = DG.featureGroup([marker1, marker2, marker3]);
+      //group.addTo(map);
+      //group.on('click', (e) => {
+      //    console.dir(group._layers);
+      //    for (let key in group._layers) {
+      //        console.dir(group._layers[key]._latlng);
+      //        //map.setView([e.latlng.lat, e.latlng.lng]);
+      //    }
+      //});
+        DG.then(function () {
+            var markers = DG.featureGroup(),
+                coordinates = [];
+      let marker1 = DG.marker([46.473025, 30.73004]).addTo(markers);
+      let marker2 = DG.marker([46.473025, 30.74104]).addTo(markers);
+      let marker3 = DG.marker([46.477225, 30.75304]).addTo(markers);
+
+            document.getElementById('hide').onclick = hideMarkers;
+            document.getElementById('show').onclick = showMarkers;
+
+            function showMarkers() {
+                markers.addTo(map);
+                map.fitBounds(markers.getBounds());
+            };
+
+            function hideMarkers() {
+                markers.removeFrom(map);
+            };
+        });
+
     }
     
 
